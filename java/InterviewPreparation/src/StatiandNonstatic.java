9dc85838593e5e6d7753f28f1452d3757556d1fb
unnimurali <tranqtesting1@gmail.com>
2018-09-17 22:04:06 +0530

    First commit

---
+++ java/InterviewPreparation/src/StatiandNonstatic.java
@@ -0,0 +1,29 @@
+
+public class StatiandNonstatic {
+	
+	int i=10;
+	static int j=20;
+	
+	public void meth1()
+	{
+		System.out.println("Non static");
+		System.out.println(j);
+	}
+
+	public static void meth2()
+	{
+		System.out.println("Static");
+		//System.out.println(i);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		meth2();
+		
+		StatiandNonstatic sn=new StatiandNonstatic();
+		System.out.println(sn.i);
+		sn.meth1();
+
+	}
+
+}
